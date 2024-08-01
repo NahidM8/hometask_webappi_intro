68457be0c3829ca967d1b71300429f67e76a2ccb
Nahid <nahidbm@code.edu.az>
2024-08-01 18:21:48 +0400

    Add project files.

---
+++ hometask_webappi_intro/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using hometask_webappi_intro.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hometask_webappi_intro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public List<Product> list = new()
+        {
+            new(){Name="Product1",Description="bbbbbbbbbbbbbbb"},
+            new(){Name="Product2",Description="bbbbbbbbbbbbbbb"},
+            new(){Name="Product3",Description="bbbbbbbbbbbbbbb"},
+            new(){Name="Product4",Description="bbbbbbbbbbbbbbb"}
+        };
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(200, list);
+        }
+    }
+}
---
+++ hometask_webappi_intro/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace hometask_webappi_intro.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
